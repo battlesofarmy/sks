9f876f7957b63dd6c55c70aff8e80eb72221620c
battlesofarmy <battlesofarmy@gmail.com>
2025-01-31 01:00:19 +0600

    update clint

--- app/academics/eeepresent/page.js
+++ app/academics/eeepresent/page.js
@@ -1,13 +1,42 @@
-import PresentDisplay from '../components/PresentDisplay'
-import axios from 'axios';
+// import PresentDisplay from '../components/PresentDisplay'
+// import axios from 'axios';
 
-export default async function EEEpresent() {
-  const res = await axios.get("https://present.muntasir3301.xyz/present/eee");
-  const phypresent = await res.data;
+// export default async function EEEpresent() {
+//   const res = await axios.get("https://present.muntasir3301.xyz/present/eee");
+//   const phypresent = await res.data;
+
+//   return (
+//     <>
+//         <PresentDisplay presents={phypresent} title={"EEE Lab Attendence"}/>
+//     </>
+//   )
+// }
+
+
+
+"use client";
+
+import { useEffect, useState } from "react";
+import PresentDisplay from "../components/PresentDisplay";
+import axios from "axios";
+
+export default function EEEpresent() {
+  const [eeepresent, setEEEPresent] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get("https://present.muntasir3301.xyz/present/eee");
+        setEEEPresent(res.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
-    <>
-        <PresentDisplay presents={phypresent} title={"EEE Lab Attendence"}/>
-    </>
-  )
+    <PresentDisplay presents={eeepresent} title={"EEE Lab Attendance"} />
+  );
 }
--- app/academics/phypresent/page.js
+++ app/academics/phypresent/page.js
@@ -1,14 +1,42 @@
-import PresentDisplay from '../components/PresentDisplay'
-import axios from 'axios';
 
-export default async function PhyPresnt() {
-  const res = await axios.get("https://present.muntasir3301.xyz/present/phy");
-  const phypresent = await res.data;
+// import PresentDisplay from '../components/PresentDisplay'
+// import axios from 'axios';
+
+// export default async function PhyPresnt() {
+//   const res = await axios.get("https://present.muntasir3301.xyz/present/phy");
+//   const phypresent = await res.data;
+
+//   return (
+//     <>
+//             <PresentDisplay presents={phypresent} title={"Physics Lab Attendence"}/>
+      
+//     </>
+//   )
+// }
+
+"use client";
+
+import { useEffect, useState } from "react";
+import PresentDisplay from "../components/PresentDisplay";
+import axios from "axios";
+
+export default function PhyPresnt() {
+  const [phypresent, setPhyPresent] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get("https://present.muntasir3301.xyz/present/phy");
+        setPhyPresent(res.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
-    <>
-            <PresentDisplay presents={phypresent} title={"Physics Lab Attendence"}/>
-      
-    </>
-  )
+    <PresentDisplay presents={phypresent} title={"Physics Lab Attendance"} />
+  );
 }
